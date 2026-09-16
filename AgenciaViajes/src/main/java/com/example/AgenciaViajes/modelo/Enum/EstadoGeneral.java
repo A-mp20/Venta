46c80163c6553b2a1765570672a1dc1c8ea0573e
Anderson murillo <110619291+A-mp20@users.noreply.github.com>
2026-09-15 23:51:11 -0500

    modificacion
    hola

--- AgenciaViajes/src/main/java/com/example/AgenciaViajes/modelo/Enum/EstadoGeneral.java
+++
@@ -1,6 +0,0 @@
-package com.example.AgenciaViajes.modelo.Enum;
-
-public enum EstadoGeneral {
-    ACTIVO,
-    INACTIVO
-}
--- AgenciaViajes/src/main/java/com/example/AgenciaViajes/modelo/Enum/TipoDocumento.java
+++
@@ -1,8 +0,0 @@
-package com.example.AgenciaViajes.modelo.Enum;
-
-public enum TipoDocumento {
-    CC,
-    CE,
-    PASAPORTE,
-    TI
-}
